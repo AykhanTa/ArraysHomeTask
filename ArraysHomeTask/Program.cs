5d517ecf940fe2b069198b326570ba5118b940e1
AykhanTa <aykhanft@code.edu.az>
2024-02-16 13:47:23 +0400

    feat:arrays home task

---
+++ ArraysHomeTask/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region Task1
+int[] numbers = {1,2,3,4,5,6};
+int sum = 0;
+for(int i = 0; i < numbers.Length; i++)
+{
+    sum+= numbers[i];
+}
+Console.WriteLine(sum);
+#endregion
